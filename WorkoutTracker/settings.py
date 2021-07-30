b141c50e4a7c95c15e257da4aea51478efba86a2
ShawneeBoy <shawnlee2001@hotmail.com>
2021-07-30 14:58:38 +0000

    Added Bootstrap

--- WorkoutTracker/settings.py
+++ WorkoutTracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Bootstrap
+    # 'bootstrap5',
     # user-made apps
     'Tracker',
 ]
