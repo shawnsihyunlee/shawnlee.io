e9d7bf7566bd57bb6a9b1b15f08bde4c0d58a269
ShawneeBoy <shawn4jworld@gmail.com>
2021-08-08 14:42:41 +0000

    Added .gitignore

--- WorkoutTracker/settings.py
+++ WorkoutTracker/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yy)yhc*j)m$r3)t&b@^v*bwhqj^9mx569g8yx6psaqt&tb=j!='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'yy)yhc*j)m$r3)t&b@^v*bwhqj^9mx569g8yx6psaqt&tb=j!=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ["workouttracker-sjcaa.run.goorm.io"]
 
